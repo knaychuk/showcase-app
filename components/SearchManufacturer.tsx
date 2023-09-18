3d9691d12b7810279e89ff4fac5581fa6312c3e9
Kate <knaychuk@gmail.com>
2023-09-18 14:42:56 -0700

    working on combobox (almost classtime!)

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { useState } from "react"
+import Image from "next/image"
+import { Combobox, Transition } from "@headlessui/react"
+
+import { SearchManufacturerProps } from "@/types"
+
+const SearchManufacturer = ({ manufacturer, setManufacturer } : SearchManufacturerProps) => {
+  const [query, setQuery] = useState('');
+
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relatvie w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image 
+              src="/car-logo.svg"
+              alt="car logo"
+              width={20}
+              height={20}
+              className="ml-4"
+            />
+          </Combobox.Button>
+
+          <Combobox.Input 
+            className="search-manufacturer__input"
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
+      </Combobox>
+      <p>{query}</p>
+    </div>
+  )
+}
+export default SearchManufacturer
