cd42e4fa0017029269a5cd2b383cb77659215708
Kate <knaychuk@gmail.com>
2023-09-25 01:10:38 -0700

    get cars from api and set as props

---
+++ components/CarCard.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { CarProps } from "@/types"
+
+import { useState } from 'react'
+import Image from "next/image"
+
+const CarCard = ({ car } : CarProps) => {
+  return (
+    <div>
+      {}
+    </div>
+  )
+}
+export default CarCard
